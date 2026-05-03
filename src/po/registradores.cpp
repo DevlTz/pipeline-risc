d1c53c8e0cc6c87e6eb93fbb1435e4345407e612
DevlTz <kauazin3339@gmail.com>
2026-05-03 05:19:05 -0300

    feat(po): implementa banco de 16 registradores com portas de leitura e escrita síncrona

--- src/po/registradores.cpp
+++ src/po/registradores.cpp
@@ -1,48 +1,21 @@
-#include <systemc.h>
+#include "registradores.h"
 
+void BANCO_REG::read_reg() {
+    sc_uint<4> reg_1 = reg_read_1.read();
+    sc_uint<4> reg_2 = reg_read_2.read();
 
-// 16 Registradores
+    value_1.write(regs[reg_1]);
+    value_2.write(regs[reg_2]);
+}
 
-SC_MODULE(BANCO_REG){
-    sc_in<sc_uint<4>> reg_read_1;
-    sc_in<sc_uint<4>> reg_read_2;
-    sc_in<sc_uint<4>> reg_write;
-    sc_in<sc_int<16>> res;
-
-    sc_in<bool> write;
-    sc_in<bool> clock;
-
-    sc_out<sc_int<16>> value_1;
-    sc_out<sc_int<16>> value_2;
-
-    sc_int<16> regs[16];
-
-    void read_reg(){
-        sc_uint<4> reg_1 = reg_read_1.read();
-        sc_uint<4> reg_2 = reg_read_2.read();
-
-        value_1.write(regs[reg_1]);
-        value_2.write(regs[reg_2]);
-    }
-
-    void write_reg(){
-        if(write.read()){
-            sc_uint<4> reg = reg_write.read();
-            sc_int<16> result = res.read();
+void BANCO_REG::write_reg() {
+    if(write.read()){
+        sc_uint<4> reg = reg_write.read();
+        sc_int<16> result = res.read();
+        
+        // Proteção opcional: evitar escrita no R0 se ele for constante zero
+        if (reg != 0) {
             regs[reg] = result;
         }
     }
-
-    SC_CTOR(BANCO_REG){
-        SC_METHOD(read_reg);
-        sensitive << reg_read_1 << reg_read_2;
-
-        SC_METHOD(write_reg);
-        sensitive << clock.pos();
-
-        // inicialização
-        for (int i = 0; i < 16; i++){
-            regs[i] = 0;
-        }
-    }
-};
+}
---
+++ src/po/registradores.h
@@ -0,0 +1,37 @@
+#ifndef REGISTRADORES_H
+#define REGISTRADORES_H
+
+#include <systemc.h>
+
+// Gerencia os 16 registradores de uso geral do processador [cite: 23]
+SC_MODULE(BANCO_REG) {
+    sc_in<sc_uint<4>> reg_read_1;
+    sc_in<sc_uint<4>> reg_read_2;
+    sc_in<sc_uint<4>> reg_write;
+    sc_in<sc_int<16>> res;
+
+    sc_in<bool> write;
+    sc_in<bool> clock;
+
+    sc_out<sc_int<16>> value_1;
+    sc_out<sc_int<16>> value_2;
+
+    sc_int<16> regs[16];
+
+    void read_reg();  // Leitura combinacional
+    void write_reg(); // Escrita síncrona
+
+    SC_CTOR(BANCO_REG) {
+        SC_METHOD(read_reg);
+        sensitive << reg_read_1 << reg_read_2;
+
+        SC_METHOD(write_reg);
+        sensitive << clock.pos(); // Escrita na borda de subida
+
+        for (int i = 0; i < 16; i++) {
+            regs[i] = 0;
+        }
+    }
+};
+
+#endif
