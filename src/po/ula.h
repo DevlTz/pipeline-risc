415dd8dce4b006388b6569f0571e638537489c24
DevlTz <kauazin3339@gmail.com>
2026-05-03 05:19:00 -0300

    feat(po): implementa ULA com suporte a operações aritméticas e lógicas RISC

---
+++ src/po/ula.h
@@ -0,0 +1,24 @@
+#ifndef ULA_H
+#define ULA_H
+
+#include <systemc.h>
+
+// Módulo responsável pelas operações matemáticas e lógicas [cite: 5-11]
+SC_MODULE(ULA) {
+    sc_in<sc_int<16>> a;
+    sc_in<sc_int<16>> b;
+    sc_in<sc_uint<4>> op;
+
+    sc_out<sc_int<16>> result;
+    sc_out<bool> flag_zero;
+    sc_out<bool> flag_negative;
+
+    void process(); // Declaração do método de processamento
+
+    SC_CTOR(ULA) {
+        SC_METHOD(process);
+        sensitive << a << b << op;
+    }
+};
+
+#endif
